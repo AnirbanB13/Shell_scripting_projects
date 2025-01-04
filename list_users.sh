fc8f66ccca1ecd0ab0d3478fb2523a82d4c35e68
AnirbanB13 <anirbanbanerjeeuv@gmail.com>
2025-01-04 12:27:34 +0000

    latest shell scripting project

---
+++ list_users.sh
@@ -0,0 +1,67 @@
+# This shell script will list users who has read access to a github repository/organisation.
+
+#############
+#Author: Anirban Banerjee
+#Date : 4th, Jan 2025
+#############
+
+#!/bin/bash
+
+#first we'll create the github API and take arguments from the user:
+
+API_URL="https://api.github.com"
+
+#github username and personal access token
+read -p "Enter your GitHub username: " USERNAME
+read -s -p "Enter your GitHub personal access token: " TOKEN
+echo
+
+#Take repository name and organisation as arguments:
+REPO_OWNER=$1
+REPO_NAME=$2
+
+#Check if the user has provided the repository name and organisation
+if [ -z "$REPO_OWNER" ] || [ -z "$REPO_NAME" ]; then
+    echo "Please provide the repository owner and name as arguments."
+    exit 1
+fi
+
+#Make a curl request to the github API to get the list of collaborators for the repository
+response=$(curl -s -u $USERNAME:$TOKEN $API_URL/repos/$REPO_OWNER/$REPO_NAME/collaborators)
+
+#Check if the curl request was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to get collaborators for the repository."
+    exit 1
+fi
+
+# Check if the response contains an error message
+if echo "$response" | jq -e '.message' > /dev/null; then
+    echo "Error: $(echo "$response" | jq -r '.message')"
+    exit 1
+fi
+
+#Parse the response to get the list of collaborators
+collaborators="$(echo $response | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+# Check if there are any collaborators
+if [ -z "$collaborators" ]; then
+    echo "No collaborators with pull access found for the repository $REPO_NAME owned by $REPO_OWNER."
+    exit 0
+fi
+
+#Print the list of collaborators
+echo "Collaborators for the repository $REPO_NAME owned by $REPO_OWNER:"
+for collaborator in $collaborators; do
+    echo "$collaborators"
+done
+
+#Exit the script
+exit 0
+
+#The script can be run from the command line with the following syntax:
+#./list_users.sh <repository_owner> <repository_name>
+#For example:
+#./list_users.sh octocat Hello-World
+
+
